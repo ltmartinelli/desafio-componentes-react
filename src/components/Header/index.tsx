5a74b811e5dd97a90e5316b285114a0c90eb280d
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-05-26 00:55:10 -0300

    Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function Header()
+{
+    return (
+        <header className="ct-header">
+            <div className="ct-container">
+                <h1>DS CarrosTop</h1>
+            </div>
+        </header>
+    );
+}
---
+++ src/routes/DSCarrosTop/index.tsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header';
+import './styles.css';
+
+export default function DSCarrosTop()
+{
+    return (
+        <>
+            <Header />
+            <main>
+        
+            </main>
+        </>
+    );
+}
